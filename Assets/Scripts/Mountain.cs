8bdbdc6ee778a0857688a3d4ba840dc7d89884ac
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2025-08-08 23:43:46 -0400

    Made script to generate bump mountain

--- Assets/Scripts/Mountain.cs
+++ Assets/Scripts/Mountain.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 public class Mountain : MonoBehaviour
 {
-    [SerializeField] private Transform bumpPrefab; // This bump prefab better not have a mountain script on it or this entire game going down
-    [SerializeField] private float spacing = 5f;
+    [SerializeField] private Transform bumpPrefab;
     [SerializeField] private float minSize = 3f;
     [SerializeField] private float maxSize = 10f;
+
+    private float spacing;
 
     private BoxCollider2D boxCollider2D;
     private SpriteRenderer spriteRenderer;
 
-    // Make sure this runs after the collider resizing script
-    private void Start()
+    private void Awake()
     {
+        spacing = minSize;
+
         boxCollider2D = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        Vector2 halfSize = boxCollider2D.size * 0.5f;
+        
+        // Box colliders using auto tiles match the size of the sprite renderer while keeping its own size at 1x1
+        Vector2 halfSize = spriteRenderer.size * 0.5f;
 
         // Local-space corners
         Vector2 localTopLeft = new Vector2(-halfSize.x, halfSize.y) + boxCollider2D.offset;
@@ -26,14 +29,18 @@ public class Mountain : MonoBehaviour
         Vector2 worldTopLeft = transform.TransformPoint(localTopLeft);
         Vector2 worldTopRight = transform.TransformPoint(localTopRight);
 
-        for (float i = 0; i < boxCollider2D.size.x; i += spacing)
-        {
-            Transform bump = Instantiate(bumpPrefab, Vector3.Lerp(worldTopLeft, worldTopRight, i / boxCollider2D.size.x), Quaternion.identity);
+        float xWidth = worldTopRight.x - worldTopLeft.x;
 
-            bump.localScale = new Vector3(Random.Range(minSize, maxSize), Random.Range(minSize, maxSize), 1f);
+        for (float i = 0; i < xWidth; i += spacing)
+        {
+            Transform bump = Instantiate(bumpPrefab, Vector3.Lerp(worldTopLeft, worldTopRight, i / xWidth), Quaternion.identity, transform); // parent to this ground so inspector is more organized
+
             bump.eulerAngles = new Vector3(0f, 0f, Random.Range(0f, 360f));
 
-            bump.GetComponent<SpriteRenderer>().color = spriteRenderer.color;
+            SpriteRenderer bumpSpriteRenderer = bump.GetComponent<SpriteRenderer>();
+
+            bumpSpriteRenderer.color = spriteRenderer.color;
+            bumpSpriteRenderer.size = new Vector2(Random.Range(minSize, maxSize), Random.Range(minSize, maxSize));
         }
     }
 }
