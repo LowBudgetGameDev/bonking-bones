a218557a06c427a0ea0e6aeefa2e5f7f6a283fb9
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2025-08-23 22:59:31 -0400

    Added drawing for face
    The code was made by chatgpt but I don't care as it is not a major part of the game.

--- Assets/Scripts/UI/PlayerSettingsUI.cs
+++ Assets/Scripts/UI/PlayerSettingsUI.cs
@@ -6,6 +6,9 @@ public class PlayerSettingsUI : MonoBehaviour
 {
     [SerializeField] private ColorPickerControl colorPicker;
     [SerializeField] private Image playerImage;
+    [SerializeField] private UIDrawing drawing;
+    [SerializeField] private RawImage faceImage;
+    [SerializeField] private Button applyButton;
 
     private void Start()
     {
@@ -13,5 +16,20 @@ public class PlayerSettingsUI : MonoBehaviour
         {
             playerImage.color = colorPicker.GetColor();
         };
+
+        drawing.OnDrawingChanged += (object sender, EventArgs e) =>
+        {
+            faceImage.texture = drawing.GetDrawing();
+        };
+
+        applyButton.onClick.AddListener(() =>
+        {
+            SaveChanges();
+        });
+    }
+
+    private void SaveChanges()
+    {
+
     }
 }
---
+++ Assets/Scripts/UI/UIDrawing.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIDrawing : MonoBehaviour
+{
+    public event EventHandler OnDrawingChanged;
+
+    [SerializeField] private Button resetButton;
+    [SerializeField] private int textureWidth = 256;
+    [SerializeField] private int textureHeight = 256;
+    [SerializeField] private int brushSize = 4;
+
+    private RawImage drawingSurface;
+
+    private Color drawColor = Color.black;
+
+    private Texture2D drawTexture;
+
+    private void Start()
+    {
+        drawingSurface = GetComponent<RawImage>();
+
+        drawTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+        ClearTexture();
+        drawTexture.wrapMode = TextureWrapMode.Clamp;
+
+        drawingSurface.texture = drawTexture;
+
+        resetButton.onClick.AddListener(() =>
+        {
+            ClearTexture();
+        });
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 localPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                drawingSurface.rectTransform,
+                Input.mousePosition,
+                null,
+                out localPos
+            );
+
+            // Convert to texture space
+            Rect rect = drawingSurface.rectTransform.rect;
+            float x = (localPos.x - rect.x) / rect.width * textureWidth;
+            float y = (localPos.y - rect.y) / rect.height * textureHeight;
+
+            DrawCircle((int)x, (int)y);
+        }
+    }
+
+    private void DrawCircle(int cx, int cy)
+    {
+        for (int x = -brushSize; x <= brushSize; x++)
+        {
+            for (int y = -brushSize; y <= brushSize; y++)
+            {
+                if (x * x + y * y <= brushSize * brushSize)
+                {
+                    int px = cx + x;
+                    int py = cy + y;
+                    if (px >= 0 && px < textureWidth && py >= 0 && py < textureHeight)
+                        drawTexture.SetPixel(px, py, drawColor);
+                }
+            }
+        }
+        drawTexture.Apply();
+
+        OnDrawingChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    public void ClearTexture()
+    {
+        Color[] clearColors = new Color[textureWidth * textureHeight];
+        for (int i = 0; i < clearColors.Length; i++) clearColors[i] = Color.clear;
+        drawTexture.SetPixels(clearColors);
+        drawTexture.Apply();
+    }
+
+    public Texture2D GetDrawing()
+    {
+        return drawTexture;
+    }
+}
